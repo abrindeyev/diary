47161ee144d9e05c9b82a782a56faf83e78a998d
Andrey Brindeyev <andrey.brindeyev@mongodb.com>
2019-12-30 12:59:29 -0800

    Role assignment bugfix

--- stitch/functions/assignAppRoles/source.js
+++ stitch/functions/assignAppRoles/source.js
@@ -3,8 +3,8 @@ exports = function(token){
 
   if (token in t2r) {
     var collection = context.services.get("mongodb-atlas").db("GITHUB_MONGO_DATABASE_NAME").collection("bv_users_metadata");
-    var res = collection.updateOne({}, {"$addToSet": {app_roles: {"$each": t2r[token] }}, "$set": {owner_id: context.user.id}}, {"upsert": true}).then( result => {
-      return { status: true, reason: result };
+    return collection.updateOne({}, {"$addToSet": {app_roles: {"$each": t2r[token] }}, "$set": {owner_id: context.user.id}}, {"upsert": true}).then( result => {
+      return { status: true, reason: result, roles: t2r[token] };
     })
     .catch(err => { return { status: false, reason: `updateOne error: ${err}`}});
   }
