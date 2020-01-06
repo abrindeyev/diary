2ba2ff94b24a36b97f1114b27c6b0bba1bd269b0
Andrey Brindeyev <andrey.brindeyev@mongodb.com>
2020-01-06 15:50:13 -0800

    Google OAuth Stitch secret deployment support
    # Conflicts:
    #	webdev.sh

--- deploy_app.sh
+++ deploy_app.sh
@@ -51,6 +51,17 @@ else
 fi
 "$cli" login --api-key "$STITCH_API_KEY" --username "$STITCH_USER"
 
+echo "Updating Stitch secrets"
+googleSecretName="$(kv googleOauthStitchSecretName)"
+googleSecretID="$("$cli" secrets list --app-id="$appId" | grep -E -v '^(ID|No secrets found for this app)' | grep "$googleSecretName"'$' | cut -f1 -d' ')"
+if [[ "$googleSecretID" != "" ]]; then
+  echo "Updating the $googleSecretName secret using the ID=$googleSecretID"
+  "$cli" secrets update --app-id="$appId" --id="$googleSecretID" --value="$(kv googleOauthStitchSecretValue)"
+else
+  echo "Creating a new $googleSecretName secret"
+  "$cli" secrets add --app-id="$appId" --name="$googleSecretName" --value="$(kv googleOauthStitchSecretValue)"
+fi
+
 # Step 1: replace the existing app
 cd "$appDir" && "$cli" import --strategy=replace --include-hosting --reset-cdn-cache --app-id="$STITCH_APP_ID" --yes
 
